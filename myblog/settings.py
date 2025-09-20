5b9da4d1c18be80e83502f4e67e018112b19ee6f
Omobowale <stigawithfun@gmail.com>
2025-09-20 13:56:59 +0100

    Updated env to switch between local and prod databases

--- myblog/settings.py
+++ myblog/settings.py
@@ -13,10 +13,20 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import environ
+
+
+# default values
+env = environ.Env(
+    DEBUG=(bool, False),
+    USE_LOCAL_DB=(bool, True)
+)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -82,14 +92,21 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+if (env('USE_LOCAL_DB')):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+else:
+    DATABASES = {
+        "default": env.db()
+    }
+    
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 
